0624fd201247488d3ec297ad4047ae7933295e63
annamusial <am@human-device.com>
2016-09-09 10:57:08 +0200

    init

---
+++ CounterApp/CounterApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Anna Musiał on 04.09.2016.
+//  Copyright © 2016 Anna Musiał. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblScore: UILabel!
+    var count=0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func buttonPressed() {
+        count++
+        lblScore.text=String(count)
+    }
+    
+    @IBAction func zerujPressed(){
+        count=0
+        lblScore.text=String(count)
+    }
+
+
+}
+
